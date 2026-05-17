741551616adef7c45469ac0b758ffd519d1e5256
12123434 <e12123434@student.tuwien.ac.at>
2026-05-17 19:06:40 +0200

    fix

---
+++ scanterior/dist/assets/AboutView-DVOe7SG_.js
@@ -0,0 +1,1 @@
+import{a as e,n as t,r as n,t as r}from"./index-CHsTQZUy.js";var i={},a={class:`about`};function o(r,i){return e(),n(`div`,a,[...i[0]||=[t(`h1`,null,`This is an about page`,-1)]])}var s=r(i,[[`render`,o]]);export{s as default};
